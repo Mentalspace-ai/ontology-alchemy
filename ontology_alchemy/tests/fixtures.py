024d46b05a14c23e23cf22478ad8688ca9703ab0
Adam Ever-Hadani <adamhadani@gmail.com>
2016-10-03 13:52:31 -0700

    skeleton unit-tests passing

--- ontology_alchemy/tests/fixtures.py
+++ ontology_alchemy/tests/fixtures.py
@@ -4,6 +4,7 @@
 RDFS_TURTLE_ONTOLOGY = """
     @prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
     @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
+    @prefix skos: <http://www.w3.org/2004/02/skos/core#> .
     @prefix exampleOntology: <http://example.com/namespace#> .
 
     exampleOntology:Thing a rdfs:Class;
--- ontology_alchemy/tests/test_ontology.py
+++ ontology_alchemy/tests/test_ontology.py
@@ -4,13 +4,14 @@ from hamcrest import (
     instance_of,
     is_,
 )
+from six import StringIO
 
 from ontology_alchemy.ontology import Ontology
 from ontology_alchemy.tests.fixtures import RDFS_TURTLE_ONTOLOGY
 
 
 def test_loading_from_file_stream_works():
-    file_object = StringIO.load(RDFS_TURTLE_ONTOLOGY)
+    file_object = StringIO(RDFS_TURTLE_ONTOLOGY)
     ontology = Ontology.load(file_object, format="turtle")
 
     assert_that(ontology, is_(instance_of(Ontology)))
